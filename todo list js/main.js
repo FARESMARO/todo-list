597b6fbeb1e5013592db3f9bd20b15642400fddd
FARESMARO <116344536+FARESMARO@users.noreply.github.com>
2023-04-27 01:01:10 +0200

    Add files via upload
    todo list ez js for beg

---
+++ todo list js/main.js
@@ -0,0 +1,25 @@
+// const addProject = document.querySelector("#input-text");
+// const contTask = document.querySelector(".list-itms");
+
+// function addTask() {
+//   if (addProject.value === "") {
+//     alert("error 404");
+//   } else {
+//     let addli = document.createElement("li");
+//     addli.innerHTML = addProject.value;
+//     contTask.appendChild(addli);
+//     let exit = document.createElement("span");
+//     exit.innerHTML = "x";
+//     addli.appendChild(exit);
+//   }
+//   addProject.value = "";
+// }
+// contTask.addEventListener("click", chekandremv);
+// function chekandremv(e) {
+//   if (e.target.TagName === "li") {
+//     e.target.className.toggle("checked");
+//   } else if (e.target.TagName === "span") {
+//     e.target.parentElement.remove();
+//   }
+//   false;
+// }
